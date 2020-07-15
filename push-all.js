4362f5775e82fec2c0ec770eb63b63aab7579aae
Davyd McColl <davydm@gmail.com>
2020-07-15 21:06:24 +0200

    :bug: should wait on result of push methods

--- push-all.js
+++ push-all.js
@@ -8,18 +8,18 @@ const
       gulpTasksGit = new Git("gulp-tasks"),
       rootGit = new Git(".");
 
-    process.stdout.write(`[ ${chalk.yellow("--")} ] ${chalk.cyanBright("pushing...")}`);
+    process.stdout.write(`${chalk.cyanBright("pushing...")}`);
     await Promise.all([
-      gulpTasksGit.push,
-      gulpTasksGit.pushTags,
-      rootGit.push,
-      rootGit.pushTags
+      gulpTasksGit.push(),
+      gulpTasksGit.pushTags(),
+      rootGit.push(),
+      rootGit.pushTags()
     ]);
-    console.log(`[ ${chalk.yellow("OK")} ] ${chalk.cyanBright("all pushed!")}`);
+    console.log(`[ ${chalk.yellow("OK")} ]`);
 
     process.exit(0);
   } catch (e) {
-    console.error(`[${chalk.redBright("FAIL")}] ${chalk.cyanBright("push fails )':")}`);
+    console.error(`[${chalk.redBright("FAIL")}]`);
     console.error(e.stack || e);
     process.exit(2);
   }
