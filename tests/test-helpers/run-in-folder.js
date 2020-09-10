37c6eb571a85b826bead81c01bfb598a25c89dc4
Davyd McColl <davydm@gmail.com>
2020-09-10 11:18:49 +0200

    :lock: upgrade yargs, should resolve warnings about outdated yargs-parser

--- tests/test-helpers/run-in-folder.js
+++ tests/test-helpers/run-in-folder.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.runInFolder = void 0;
 async function runInFolder(folder, fn) {
     const start = process.cwd(), alreadyThere = start === folder;
     if (!alreadyThere) {
