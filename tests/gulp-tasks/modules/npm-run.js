844b935e978951a6b7923a3a58844621b428d72b
Davyd McColl <davydm@gmail.com>
2021-02-03 14:42:29 +0200

    :bookmark: bump package version

--- tests/gulp-tasks/modules/npm-run.js
+++ tests/gulp-tasks/modules/npm-run.js
@@ -1,13 +1,31 @@
 "use strict";
 (function () {
     const os = require("os"), spawn = requireModule("spawn");
-    async function npmRun(args) {
+    function flatten(a) {
+        // essentially Array.flat, but I'm not assuming that's
+        // available
+        return a.reduce((acc, cur) => {
+            if (Array.isArray(cur)) {
+                return acc.concat(cur);
+            }
+            else {
+                acc.push(cur);
+                return acc;
+            }
+        }, []);
+    }
+    async function npmRun(...args) {
+        const flattened = flatten(args);
         if (os.platform() === "win32") {
-            await spawn("cmd", ["/c", "npm"].concat(args));
+            await spawn("cmd", ["/c", "npm"].concat(flattened));
         }
         else {
-            await spawn("npm", args);
+            await spawn("npm", flattened);
         }
+    }
+    async function foo() {
+        await npmRun("foo", "bar");
+        await npmRun(["foo", "bar"]);
     }
     module.exports = {
         npmRun
