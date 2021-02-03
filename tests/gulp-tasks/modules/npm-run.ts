1c3d29e5b506407f81da0f21c48021ac88b60d1e
Davyd McColl <davydm@gmail.com>
2021-02-03 14:41:18 +0200

    :sparkles: call npmRun with an array or just a bunch of args

--- tests/gulp-tasks/modules/npm-run.ts
+++ tests/gulp-tasks/modules/npm-run.ts
@@ -3,12 +3,34 @@
     os = require("os"),
     spawn = requireModule<Spawn>("spawn");
 
-  async function npmRun(args: string[]): Promise<void> {
+  type StringOrArrayOfStrings = string | string[];
+
+  function flatten(a: StringOrArrayOfStrings[]) {
+    // essentially Array.flat, but I'm not assuming that's
+    // available
+    return a.reduce(
+      (acc: string[], cur: StringOrArrayOfStrings) => {
+        if (Array.isArray(cur)) {
+          return acc.concat(cur);
+        } else {
+          acc.push(cur);
+          return acc;
+        }
+      }, [] as string[]);
+  }
+
+  async function npmRun(...args: StringOrArrayOfStrings[]): Promise<void> {
+    const flattened = flatten(args);
     if (os.platform() === "win32") {
-      await spawn("cmd", [ "/c", "npm" ].concat(args))
+      await spawn("cmd", [ "/c", "npm" ].concat(flattened))
     } else {
-      await spawn("npm", args);
+      await spawn("npm", flattened);
     }
+  }
+
+  async function foo() {
+    await npmRun("foo", "bar");
+    await npmRun([ "foo", "bar"]);
   }
 
   module.exports = {
