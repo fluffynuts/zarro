2b4ebf0487f097d5e1febf0cf74b11efa333fe95
Davyd McColl <davydm@gmail.com>
2022-06-07 16:50:13 +0200

    :sparkles: allow filtering on --show-env

--- index-modules/handlers/show-env.js
+++ index-modules/handlers/show-env.js
@@ -1,10 +1,12 @@
 const
   chalk = require("ansi-colors"),
   invokeGulp = require("./invoke-gulp").handler,
+  longSwitch = "--show-environment",
+  shortSwitch = "--show-env",
   contains = require("../contains-any");
 
 module.exports = {
-  test: args => contains(args, "--show-environment", "--show-env"),
+  test: args => contains(args, longSwitch, shortSwitch),
   handler: () => {
     console.log(chalk.whiteBright("-- Zarro Environment Variables --"));
     console.log(chalk.gray(
@@ -14,6 +16,21 @@ module.exports = {
       ].join("\n")
       )
     )
+    const filter = [];
+    let addFilter = false;
+    for (const arg of process.argv) {
+      switch (arg) {
+        case longSwitch:
+        case shortSwitch:
+          addFilter = true;
+          break;
+        default:
+          if (addFilter) {
+            filter.push(arg);
+          }
+      }
+    }
+    process.env["HELP_ENV_FILTER"] = filter.join(" ");
     // next, spawn gulp with the help:environment task
     // -> we do this to ensure that any built-in or local task which
     //    has registered usage of variables has its registration
