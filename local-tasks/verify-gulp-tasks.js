efd9e49fb5c8e56746aad06aae585a2fce5a99d7
Davyd McColl <davydm@gmail.com>
2023-08-25 15:39:50 +0200

    :bookmark: bump package version to 1.144.106

--- local-tasks/verify-gulp-tasks.js
+++ local-tasks/verify-gulp-tasks.js
@@ -1,28 +1,23 @@
 "use strict";
 // import path from "path";
 (function () {
-    // const
-    //   gitFactory = require("simple-git"),
-    //   gulp = requireModule<Gulp>("gulp");
-    //
-    // gulp.task("verify-gulp-tasks", async () => {
-    //   // try {
-    //   //   if (process.env.RUNNING_IN_GITHUB_ACTION) {
-    //   //     return;
-    //   //   }
-    //   //   const
-    //   //     at = path.resolve(path.join(__dirname, "..", "gulp-tasks")),
-    //   //     git = gitFactory(at),
-    //   //     expected = "master",
-    //   //     branchInfo = await git.branch();
-    //   //   if (!branchInfo || branchInfo.current !== expected) {
-    //   //     const err = `Expected gulp-tasks to be checked out as master`;
-    //   //     console.error(err);
-    //   //     throw new Error(err);
-    //   //   }
-    //   // } catch (e) {
-    //   //   console.error(`can't verify gulp-tasks: ${(e as any).toString()}`);
-    //   //   throw e;
-    //   // }
-    // });
+    const gulp = requireModule("gulp");
+    gulp.task("verify-gulp-tasks", async () => {
+        const gitFactory = require("simple-git"), path = require("path");
+        try {
+            if (process.env.RUNNING_IN_GITHUB_ACTION) {
+                return;
+            }
+            const at = path.resolve(path.join(__dirname, "..", "gulp-tasks")), git = gitFactory(at), expected = "master", branchInfo = await git.branch();
+            if (!branchInfo || branchInfo.current !== expected) {
+                const err = `Expected gulp-tasks to be checked out as master`;
+                console.error(err);
+                throw new Error(err);
+            }
+        }
+        catch (e) {
+            console.error(`can't verify gulp-tasks: ${e.toString()}`);
+            throw e;
+        }
+    });
 })();
