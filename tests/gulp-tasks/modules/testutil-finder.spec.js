d20a71b74347f1beb1eaf32fba5f384a75630377
Davyd McColl <davydm@gmail.com>
2023-09-08 10:38:49 +0200

    :bug: findTool should return the absolute path to the tool

--- tests/gulp-tasks/modules/testutil-finder.spec.js
+++ tests/gulp-tasks/modules/testutil-finder.spec.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("expect-even-more-jest");
+const filesystem_sandbox_1 = require("filesystem-sandbox");
 describe(`testutil-finder`, () => {
     describe(`compareVersionArrays`, () => {
         const { compareVersionArrays } = requireModule("testutil-finder");
@@ -47,6 +48,42 @@ describe(`testutil-finder`, () => {
                         .toEqual(e);
                 });
             });
+        });
+    });
+    describe(`findTool`, () => {
+        const { findTool } = requireModule("testutil-finder");
+        const which = requireModule("which");
+        it(`should return undefined when not found at all`, async () => {
+            // Arrange
+            const sandbox = await filesystem_sandbox_1.Sandbox.create();
+            // Act
+            const result = findTool("foo.exe", sandbox.path);
+            // Assert
+            expect(result)
+                .toBeUndefined();
+        });
+        it(`should return the tool in the path when only found in path`, async () => {
+            // Arrange
+            const sandbox = await filesystem_sandbox_1.Sandbox.create(), expected = which("git");
+            // Act
+            const result = findTool("git", sandbox.path);
+            // Assert
+            expect(result)
+                .toEqual(expected);
+        });
+        it(`should return the full path to the found tool`, async () => {
+            // Arrange
+            const { copyFile } = require("yafs"), path = require("path"), sandbox = await filesystem_sandbox_1.Sandbox.create(), original = which("git");
+            if (!original) {
+                console.warn("this test requires git to be in the path");
+                return;
+            }
+            await copyFile(original, sandbox.path);
+            // Act
+            const result = findTool("git", sandbox.path);
+            // Assert
+            expect(result)
+                .toEqual(sandbox.fullPathFor(path.basename(original)));
         });
     });
 });
--- tests/gulp-tasks/modules/testutil-finder.spec.ts
+++ tests/gulp-tasks/modules/testutil-finder.spec.ts
@@ -1,68 +1,115 @@
 import "expect-even-more-jest";
+import { Sandbox } from "filesystem-sandbox";
 
 describe(`testutil-finder`, () => {
-  describe(`compareVersionArrays`, () => {
-    const
-      { compareVersionArrays } = requireModule<TestUtilFinder>("testutil-finder");
-    it(`should return zero for two identical versions`, async () => {
-      // Arrange
-      const
-        x = [ 1, 2, 3 ],
-        y = [ 1, 2, 3 ];
-      // Act
-      const result = compareVersionArrays(x, y);
-      // Assert
-      expect(result)
-        .toEqual(0);
-    });
-
-    it(`should return -1 when x > y`, async () => {
-      // Arrange
-      const
-        x = [ 2, 0, 0 ],
-        y = [ 1, 5, 6 ];
-      // Act
-      const result = compareVersionArrays(x, y);
-      // Assert
-      expect(result)
-        .toEqual(-1);
-    });
-
-    it(`should return 1 when x < y`, async () => {
-      // Arrange
-      const
-        x = [ 1, 4, 9 ],
-        y = [ 1, 5, 1 ];
-      // Act
-      const result = compareVersionArrays(x, y);
-      // Assert
-      expect(result)
-        .toEqual(1);
-    });
-
-    describe(`edge cases`, () => {
-      interface TestCase {
-        x: number[],
-        y: number[],
-        e: 1 | 0 | -1
-      }
-
-      const testCases =
-        [
-          { x: [ 1, 4, 9 ], y: [ 1, 5, 0, 1, 2 ], e: 1 },
-          { x: [ 1, 2, 3, 4, 5 ], y: [ 1, 3 ], e: 1 }
-        ] as TestCase[];
-      testCases.forEach((tc: TestCase) => {
-        it(`should ignore version numbers in y but not in x`, async () => {
-          // Arrange
-          const { x, y, e } = tc;
-          // Act
-          const result = compareVersionArrays(x, y);
-          // Assert
-          expect(result)
-            .toEqual(e);
+    describe(`compareVersionArrays`, () => {
+        const
+            { compareVersionArrays } = requireModule<TestUtilFinder>("testutil-finder");
+        it(`should return zero for two identical versions`, async () => {
+            // Arrange
+            const
+                x = [ 1, 2, 3 ],
+                y = [ 1, 2, 3 ];
+            // Act
+            const result = compareVersionArrays(x, y);
+            // Assert
+            expect(result)
+                .toEqual(0);
         });
-      });
+
+        it(`should return -1 when x > y`, async () => {
+            // Arrange
+            const
+                x = [ 2, 0, 0 ],
+                y = [ 1, 5, 6 ];
+            // Act
+            const result = compareVersionArrays(x, y);
+            // Assert
+            expect(result)
+                .toEqual(-1);
+        });
+
+        it(`should return 1 when x < y`, async () => {
+            // Arrange
+            const
+                x = [ 1, 4, 9 ],
+                y = [ 1, 5, 1 ];
+            // Act
+            const result = compareVersionArrays(x, y);
+            // Assert
+            expect(result)
+                .toEqual(1);
+        });
+
+        describe(`edge cases`, () => {
+            interface TestCase {
+                x: number[],
+                y: number[],
+                e: 1 | 0 | -1
+            }
+
+            const testCases =
+                [
+                    { x: [ 1, 4, 9 ], y: [ 1, 5, 0, 1, 2 ], e: 1 },
+                    { x: [ 1, 2, 3, 4, 5 ], y: [ 1, 3 ], e: 1 }
+                ] as TestCase[];
+            testCases.forEach((tc: TestCase) => {
+                it(`should ignore version numbers in y but not in x`, async () => {
+                    // Arrange
+                    const { x, y, e } = tc;
+                    // Act
+                    const result = compareVersionArrays(x, y);
+                    // Assert
+                    expect(result)
+                        .toEqual(e);
+                });
+            });
+        });
     });
-  });
+
+    describe(`findTool`, () => {
+        const { findTool } = requireModule<TestUtilFinder>("testutil-finder");
+        const which = requireModule<Which>("which");
+        it(`should return undefined when not found at all`, async () => {
+            // Arrange
+            const
+                sandbox = await Sandbox.create();
+            // Act
+            const result = findTool("foo.exe", sandbox.path);
+            // Assert
+            expect(result)
+                .toBeUndefined();
+        });
+
+        it(`should return the tool in the path when only found in path`, async () => {
+            // Arrange
+            const
+                sandbox = await Sandbox.create(),
+                expected = which("git");
+            // Act
+            const result = findTool("git", sandbox.path);
+            // Assert
+            expect(result)
+                .toEqual(expected);
+        });
+
+        it(`should return the full path to the found tool`, async () => {
+            // Arrange
+            const
+                { copyFile } = require("yafs"),
+                path = require("path"),
+                sandbox = await Sandbox.create(),
+                original = which("git");
+            if (!original) {
+                console.warn("this test requires git to be in the path");
+                return;
+            }
+            await copyFile(original, sandbox.path);
+            // Act
+            const result = findTool("git", sandbox.path);
+            // Assert
+            expect(result)
+                .toEqual(sandbox.fullPathFor(path.basename(original)));
+        });
+    });
 });
