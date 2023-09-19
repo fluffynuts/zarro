c602a5f746434404a9f6a32beb115f7f3eefaba4
Davyd McColl <davydm@gmail.com>
2023-09-19 11:05:25 +0200

    :wrench: ensure root is pushed after gulp-tasks: looks like sometimes root is pushed first and the GHA side fails to find gulp-tasks@{sha}

--- push-all.js
+++ push-all.js
@@ -1,25 +1,27 @@
 const
-  chalk = require("./gulp-tasks/modules/ansi-colors"),
+  { cyanBright, yellow, redBright } = require("./gulp-tasks/modules/ansi-colors"),
   gitFactory = require("simple-git");
 
-(async function () {
+(async function (){
   try {
     const
       gulpTasksGit = gitFactory("gulp-tasks"),
       rootGit = gitFactory(".");
 
-    process.stdout.write(`${chalk.cyanBright("pushing...")}`);
+    process.stdout.write(`${ cyanBright("pushing...") }`);
     await Promise.all([
       gulpTasksGit.push(),
       gulpTasksGit.pushTags(),
+    ]);
+    await Promise.all([
       rootGit.push(),
       rootGit.pushTags()
     ]);
-    console.log(`[ ${chalk.yellow("OK")} ]`);
+    console.log(`[ ${ yellow("OK") } ]`);
 
     process.exit(0);
   } catch (e) {
-    console.error(`[${chalk.redBright("FAIL")}]`);
+    console.error(`[${ redBright("FAIL") }]`);
     console.error(e.stack || e);
     process.exit(2);
   }
