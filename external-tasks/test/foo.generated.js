4cd86ce6a5df1e8f6d968e9e54d1f6247a76a51f
Davyd McColl <davydm@gmail.com>
2023-09-20 10:11:10 +0200

    :bookmark: bump package version to 1.144.275

--- external-tasks/test/foo.generated.js
+++ external-tasks/test/foo.generated.js
@@ -1,8 +1,9 @@
-/// <reference path="../../../node_modules/zarro/types.d.ts" />
-const gulp = requireModule("gulp");
-gulp.task("external", async () => {
-    const sleep = requireModule("sleep");
-    console.log("external task starts");
-    await sleep(1000);
-    console.log("external task done");
-});
+(function () {
+    const gulp = requireModule("gulp");
+    gulp.task("external", async () => {
+        const sleep = requireModule("sleep");
+        console.log("external task starts");
+        await sleep(1000);
+        console.log("external task done");
+    });
+})();
