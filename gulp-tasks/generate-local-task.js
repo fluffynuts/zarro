7471e29dde71e7d75e43be53f53da8d24d9b270b
Davyd McColl <davydm@gmail.com>
2023-12-08 13:18:23 +0200

    :bookmark: bump package version to 1.160.0

--- gulp-tasks/generate-local-task.js
+++ gulp-tasks/generate-local-task.js
@@ -2,11 +2,12 @@
 (function () {
     const gulp = requireModule("gulp");
     gulp.task("generate-local-task", async () => {
-        const path = require("path"), { ask } = requireModule("ask"), rawName = await ask("name of new task"), { writeTextFile, fileExists } = require("yafs"), taskName = rawName.trim().replace(/\s+/g, "-"), target = path.join("local-tasks", `${taskName}.ts`);
+        const log = requireModule("log"), path = require("path"), { ask } = requireModule("ask"), rawName = await ask("name of new task"), { writeTextFile, fileExists } = require("yafs"), taskName = rawName.trim().replace(/\s+/g, "-"), target = path.join("local-tasks", `${taskName}.ts`);
         if (await fileExists(target)) {
             throw new Error(`File already exists at '${target}' - remove it first or rename your task`);
         }
         await writeTextFile(target, template.replace("%TASK_NAME%", taskName));
+        log.info(`generated new task file at: ${target}`);
     });
     const template = `
 /// <reference path="../node_modules/zarro/types.d.ts" />
