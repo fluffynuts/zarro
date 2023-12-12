3112b10068b60500144471f2ced459d4ffa04958
Davyd McColl <davydm@gmail.com>
2023-12-12 09:11:41 +0200

    :bookmark: bump package version to 1.165.4

--- gulp-tasks/modules/fetch-github-release.js
+++ gulp-tasks/modules/fetch-github-release.js
@@ -1,9 +1,19 @@
 "use strict";
 (async function () {
     requireModule("fetch");
-    const path = require("path"), { folderExistsSync } = require("yafs");
-    const imported = folderExistsSync(path.join(__dirname, "fetch-github-release", "dist"))
-        ? require("./fetch-github-release/dist")
-        : require("./fetch-github-release/src");
-    module.exports = imported;
+    const path = require("path"), { folderExistsSync } = require("yafs"), 
+    // ideally, during dev, we want fresh src files
+    // but the build produces a dist folder with
+    // js artifacts, which also work - but npm
+    // refuses to pack them, and I haven't figured out
+    // why; so another process should create dist-copy
+    // specifically for packing purposes
+    search = ["src", "dist", "dist-copy"];
+    for (const item of search) {
+        const seek = path.join(__dirname, "fetch-github-release", item);
+        if (folderExistsSync(seek)) {
+            module.exports = require(seek);
+            return;
+        }
+    }
 })();
