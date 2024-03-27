c6afe66f22c1de4a789b2b6174fe60e61d8c0bed
Davyd McColl <davydm@gmail.com>
2024-03-27 16:20:00 +0200

    :alembic: provide generic nuget-push task which should push the latest of each package found in PACK_TARGET_FOLDER

---
+++ gulp-tasks/nuget-push.ts
@@ -0,0 +1,40 @@
+(function () {
+  gulp.task(
+    "nuget-push",
+    "Pushes the latest versions of packages in the package build dir",
+    async () => {
+      const
+        debug = requireModule<DebugFactory>("debug")(__filename),
+        path = require("path"),
+        nugetPush = requireModule<NugetPush>("nuget-push"),
+        {
+          ls,
+          FsEntities
+        } = require("yafs"),
+        env = requireModule<Env>("env"),
+        folder = env.resolve(env.PACK_TARGET_FOLDER),
+        versionRe = /^(?<id>[A-Za-z\.]+)\.(?<version>\d\.\d\.\d)(-(?<tag>.*))?.nupkg$/,
+        packages = await ls(folder, {
+          recurse: false,
+          entities: FsEntities.files,
+          match: versionRe
+        }),
+        sorted = packages.sort().reverse(),
+        seen = new Set<string>();
+      for (const file of sorted) {
+        const
+          match = file.match(versionRe),
+          id = match?.groups["id"];
+        if (seen.has(id)) {
+          debug(`already seen ${ id }, skipping ${ file }`);
+          continue;
+        }
+        seen.add(id);
+
+        await nugetPush(
+          path.join(folder, file)
+        );
+      }
+    }
+  );
+})();
