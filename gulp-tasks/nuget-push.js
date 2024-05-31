f72f25f5d8f793172fe0859b37bb53e36b04e24f
Davyd McColl <davydm@gmail.com>
2024-05-31 13:55:31 +0200

    :bookmark: bump package version to 1.176.1

--- gulp-tasks/nuget-push.js
+++ gulp-tasks/nuget-push.js
@@ -1,7 +1,7 @@
 "use strict";
 (function () {
     gulp.task("nuget-push", "Pushes the latest versions of packages in the package build dir", async () => {
-        const debug = requireModule("debug")(__filename), path = require("path"), nugetPush = requireModule("nuget-push"), { ls, FsEntities } = require("yafs"), env = requireModule("env"), folder = env.resolve(env.PACK_TARGET_FOLDER), versionRe = /^(?<id>[A-Za-z\.]+)\.(?<version>\d\.\d\.\d)(-(?<tag>.*))?\.nupkg$/, packages = await ls(folder, {
+        const { ctx } = require("exec-step"), debug = requireModule("debug")(__filename), path = require("path"), nugetPush = requireModule("nuget-push"), { ls, FsEntities } = require("yafs"), env = requireModule("env"), folder = env.resolve(env.PACK_TARGET_FOLDER), versionRe = /^(?<id>[A-Za-z\.]+)\.(?<version>\d\.\d\.\d)(-(?<tag>.*))?\.nupkg$/, packages = await ls(folder, {
             recurse: false,
             entities: FsEntities.files,
             match: versionRe
@@ -28,7 +28,7 @@
             return;
         }
         for (const file of toPush) {
-            await nugetPush(path.join(folder, file));
+            await ctx.exec(`⬆️ pushing ${file}`, async () => await nugetPush(path.join(folder, file)));
         }
     });
 })();
