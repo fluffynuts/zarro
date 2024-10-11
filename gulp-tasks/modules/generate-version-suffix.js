e199f71c040f118d5b8408d2629e21acd447645a
Davyd McColl <davydm@gmail.com>
2024-10-11 13:14:56 +0200

    :bookmark: bump package version to 1.187.0

--- gulp-tasks/modules/generate-version-suffix.js
+++ gulp-tasks/modules/generate-version-suffix.js
@@ -2,7 +2,11 @@
 (function () {
     const { currentShortSHA } = requireModule("git-sha");
     const { timestamp } = requireModule("timestamp");
+    const options = {
+        fullYear: false,
+        includeSeconds: false
+    };
     module.exports = function generateVersionSuffix() {
-        return `${timestamp({ fullYear: false })}.${currentShortSHA()}`;
+        return `${timestamp(options)}.${currentShortSHA()}`;
     };
 })();
