cb5885e3bb6658dda0d320857a1dd997c56d8479
Davyd McColl <davydm@gmail.com>
2025-09-03 11:01:52 +0200

    :bookmark: bump package version to 1.203.0

--- gulp-tasks/modules/test-dotnet-logic.js
+++ gulp-tasks/modules/test-dotnet-logic.js
@@ -284,7 +284,9 @@ Test Run Summary
                 continue;
             }
             if (next) {
-                return path.basename(arg).replace(/\.dll$/i, "");
+                return path.basename(arg)
+                    .replace(/\.dll$/i, "")
+                    .replace(/\.csproj$/i, "");
             }
         }
         return `(project name parse failed for:) ${args.join(" ")}`;
