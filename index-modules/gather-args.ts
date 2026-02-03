a7057ad4a3d48be1a22aef00584b8b9954173775
Davyd McColl <davydm@gmail.com>
2026-02-03 10:02:01 +0200

    :alembic: fall back on trying to identify the zarro entrypoint from argv

--- index-modules/gather-args.ts
+++ index-modules/gather-args.ts
@@ -1,4 +1,5 @@
 (function() {
+    const debug = requireModule<DebugFactory>("debug")(__filename);
     module.exports = async function gatherArgs(
         potentialEntryPoints: string | string[],
         overrideArgv?: string[] // for testing only
@@ -12,6 +13,12 @@
             if (entryPointIndex > -1) {
                 return argv.slice(entryPointIndex + 1);
             }
+        }
+        for (let arg of argv) {
+          if (arg.match(/node_modules\/zarro\/index.js/)) {
+            debug(`using entrypoint from argv: ${arg}`);
+            return arg;
+          }
         }
         throw new Error(`Can't figure out args: unable to find entry point in args list:\n${ JSON.stringify(
             {
